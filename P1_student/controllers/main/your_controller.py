83bcba30bfe569a927640c6f47bc32c7505e9411
IshanGupta <106355001+IGfun2code@users.noreply.github.com>
2025-11-07 10:22:17 -0500

    Completed Project 1 where I use PID control and a bit of foresight to control the heading of the car.

--- P1_student/controllers/main/your_controller.py
+++ P1_student/controllers/main/your_controller.py
@@ -22,6 +22,31 @@ class CustomController(BaseController):
         self.m = 1888.6
         self.g = 9.81
 
+        # PID tuning values for lateral controller ()
+        self.kp_lat = 4.5
+        self.ki_lat = 0.6
+        self.kd_lat = 0.5
+
+        # Set the number of steps to look ahead
+        self.look_ahead = 150
+
+        # integral history term
+        self.integral_error_heading = 0.0
+        # derivative prev term
+        self.prev_error_heading = 0.0
+
+        # PID tuning values for longitude controller (speed of vehicle)
+        self.target_vel = 12 #m/s
+        self.angle_diff = np.pi/6
+        self.slow_down = 10.0
+        self.kp_vel = 800
+        self.ki_vel = 100
+        self.kd_vel = 50
+        # integral history term
+        self.integral_error_vel = 0.0
+        # derivative prev error
+        self.prev_error_vel = 0.0
+
         # Add additional member variables according to your need here.
 
     def update(self, timestep):
@@ -43,32 +68,41 @@ class CustomController(BaseController):
         # to calculate control inputs (F, delta). 
 
         # ---------------|Lateral Controller|-------------------------
-        """
-        Please design your lateral controller below.
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        """
+        # get the desired trajectory X steps ahead
+        distance, near_index = closestNode(X, Y, trajectory)
+        # wrap the index if it goes over the index of the trajectory list
+        look_index = int((near_index + self.look_ahead) % len(trajectory))
+        # get target X and Y positions
+        target_XY = self.trajectory[look_index]
+
+        # Determine heading from the target X and Y
+        desired_heading = np.arctan2(target_XY[1] - Y, target_XY[0] - X)
+
+        # Get error from heading
+        heading_error = wrapToPi(desired_heading - psi)
+        self.integral_error_heading += heading_error * delT
+        der_error_heading = (heading_error - self.prev_error_heading)/(delT + 1e-10)
+
+        #store the current heading error in prev_error_heading
+        self.prev_error_heading = heading_error
+
+        # get the new delta value
+        delta = self.kp_lat * heading_error + self.ki_lat * self.integral_error_heading + self.kd_lat * der_error_heading
 
         # ---------------|Longitudinal Controller|-------------------------
-        """
-        Please design your longitudinal controller below.
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        .
-        """
+        if np.abs(heading_error) > self.angle_diff:
+            velocity = self.target_vel - self.slow_down
+        else:
+            velocity = self.target_vel
+        vel_error = velocity - xdot
+        self.integral_error_vel += vel_error * delT
+        der_error_vel = (vel_error - self.prev_error_vel)/(delT + 1e-10)
+
+        # store the current vel in prev_error_vel
+        self.prev_error_vel = vel_error
+
+        #get the new F value:
+        F = self.kp_vel * vel_error + self.ki_vel * self.integral_error_vel + self.kd_vel * der_error_vel
 
         # Return all states and calculated control inputs (F, delta)
         return X, Y, xdot, ydot, psi, psidot, F, delta
